1e344d930df5a05fb3bd978e18809e9e8ed9a2ae
kmj <milcan@knou.ac.kr>
2023-01-11 08:49:45 +0900

    2차커밋

--- src/main/java/com/awsweb/book/springboot/web/dto/indexController.java
+++ src/main/java/com/awsweb/book/springboot/web/dto/indexController.java
@@ -1,5 +1,6 @@
 package com.awsweb.book.springboot.web.dto;
 
+import com.awsweb.book.springboot.config.auth.dto.SessionUser;
 import com.awsweb.book.springboot.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,15 +8,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class indexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user"); // 로그인 성공 시 SessionUser 저장
+        if ( user != null) {     // 세션에 저장된 값이 있을 때만 model에 user Name으로 등록.
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
